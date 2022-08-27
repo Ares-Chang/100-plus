13accfebb7384a14effbfbd89484df743b5a8095
chang <2622310479@qq.com>
2022-08-27 19:38:05 +0800

    chore: change GitHub Repository Link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,7 +7,7 @@
     <a
       icon-btn i-carbon-logo-github
       rel="noreferrer"
-      href="https://github.com/antfu/vitesse-lite"
+      href="https://github.com/Ares-Chang/calendar-clock"
       target="_blank"
       title="GitHub"
     />
