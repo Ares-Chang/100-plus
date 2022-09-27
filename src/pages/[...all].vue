337015e77efdb0a92d659a7b9c215891cbf4ebc4
chang <2622310479@qq.com>
2022-09-27 16:46:45 +0800

    feat: created 404 page

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    Not Found
-  </div>
+  <n-card w-200="!" text-center mx-auto hoverable>
+    <n-gradient-text
+      size="40"
+      type="error"
+      font-bold="!"
+    >
+      404 Not Found
+      <br>
+      从前有座山，山上有座庙
+      <br>
+      庙里有个页面，现在找不到
+    </n-gradient-text>
+  </n-card>
 </template>
