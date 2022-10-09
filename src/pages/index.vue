9df67447d101eb69f59095dd2e32475b086ca086
chang <2622310479@qq.com>
2022-10-09 14:00:32 +0800

    style: remove global padding and footer module

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,12 +4,15 @@ const value = ref(addDays(Date.now(), 1).valueOf())
 </script>
 
 <template>
-  <div>
-    <NCalendar
-      v-model:value="value"
-      #="{ year, month, date }"
-    >
-      {{ year }}-{{ month }}-{{ date }}
-    </NCalendar>
+  <div p="x-4 y-10">
+    <div>
+      <NCalendar
+        v-model:value="value"
+        #="{ year, month, date }"
+      >
+        {{ year }}-{{ month }}-{{ date }}
+      </NCalendar>
+    </div>
+    <Footer />
   </div>
 </template>
