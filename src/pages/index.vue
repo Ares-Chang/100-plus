41134eec489df5b82c7fe649d72908036cf7ab22
chang <2622310479@qq.com>
2022-10-11 17:33:36 +0800

    style: homepage fixed NavNar

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,25 +4,30 @@ const value = ref(addDays(Date.now(), 1).valueOf())
 </script>
 
 <template>
-  <n-layout>
+  <n-layout
+    h-100vh
+    content-style="display: flex; flex-flow: column;"
+  >
     <n-layout-header
       bordered
     >
       <NavBar />
     </n-layout-header>
-    <n-layout-content p-8>
-      <div flex justify-center items-center>
-        <NCalendar
-          v-model:value="value"
-          w80vw h="90vh!"
-          #="{ year, month, date }"
-        >
-          {{ year }}-{{ month }}-{{ date }}
-        </NCalendar>
-      </div>
-    </n-layout-content>
-    <n-layout-footer bordered>
-      <Footer />
-    </n-layout-footer>
+    <n-layout>
+      <n-layout-content p-8>
+        <div flex justify-center items-center>
+          <NCalendar
+            v-model:value="value"
+            w80vw h="90vh!"
+            #="{ year, month, date }"
+          >
+            {{ year }}-{{ month }}-{{ date }}
+          </NCalendar>
+        </div>
+      </n-layout-content>
+      <n-layout-footer bordered>
+        <Footer />
+      </n-layout-footer>
+    </n-layout>
   </n-layout>
 </template>
