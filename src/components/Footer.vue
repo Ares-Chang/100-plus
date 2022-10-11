45c27d07905b38f68e6eb7c601f826ad86849282
chang <2622310479@qq.com>
2022-10-11 17:12:07 +0800

    fix: 适配 Footer 组件主题切换导致文字不清

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div p-6 text-center text="#EBEBEB99" text-4>
+  <div p-6 text-center dark:text="#ebebeb99" text="#383838b3" text-4>
     <p>Released under the MIT License.</p>
     <p>Copyright © 2022-PRESENT Ares Chang</p>
   </div>
