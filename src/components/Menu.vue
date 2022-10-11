c8a8f3c68113b4b4b7d41b3abf0668c96ba6e978
chang <2622310479@qq.com>
2022-10-11 16:58:42 +0800

    feat: create NavBar component

---
+++ src/components/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav
+    text-xl text-center
+    flex items-center
+  >
+    <a
+      mx-2
+      icon-btn i-carbon:time
+      href="/time"
+      title="Time"
+    />
+
+    <button mx-2 icon-btn @click="toggleDark()">
+      <div dark:i-carbon-moon i-carbon-sun />
+    </button>
+
+    <a
+      mx-2
+      icon-btn i-carbon-logo-github
+      rel="noreferrer"
+      href="https://github.com/Ares-Chang/calendar-clock"
+      target="_blank"
+      title="GitHub"
+    />
+  </nav>
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    flex justify-between items-center
+    p-3
+  >
+    <Logo />
+    <Menu />
+  </div>
+</template>
