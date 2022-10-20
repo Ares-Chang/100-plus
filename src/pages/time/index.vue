81a4650ebf71243ed22d57a8670e58cad6454484
chang <2622310479@qq.com>
2022-10-20 15:51:40 +0800

    feat: show random clock project page

--- src/pages/time/index.vue
+++ src/pages/time/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 useSetTitle('Clock')
+/**
+ * show random clock project page
+ */
+const num = useRandomInt(1, 2)
 </script>
 
 <template>
-  <iframe w-100vw h-100vh src="/time/1" frameborder="0" />
+  <iframe w-100vw h-100vh :src="`/time/${num}`" frameborder="0" />
 </template>
 
 <route lang="yaml">
