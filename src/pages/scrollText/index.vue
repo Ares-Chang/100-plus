51c78adbe351346b6092ec2896a05e3b032d049a
chang <2622310479@qq.com>
2023-01-15 17:55:22 +0800

    feat: scroll-text 页面构建

---
+++ src/pages/scrollText/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const text = ref('我是一段很长很长的文字，你看我长不长')
+</script>
+
+<template>
+  <div bg-black w-100vw h-100vh flex justify-center items-center overflow-hidden>
+    <div class="LoopText">
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.LoopText {
+  @apply text-8xl;
+  white-space: nowrap;
+  animation: 4s LoopText linear infinite normal;
+}
+@keyframes LoopText {
+  0% {
+    transform: rotate(90deg) translateX(0px);
+  }
+  100% {
+    transform: rotate(90deg) translateX(-100%);
+  }
+}
+</style>
