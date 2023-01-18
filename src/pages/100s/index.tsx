0b2cb3223b1e917f79ff83317539cf9c5790df8c
chang <2622310479@qq.com>
2023-01-18 14:19:56 +0800

    feat(100s): 添加主题切换功能

--- src/pages/100s/index.tsx
+++ src/pages/100s/index.tsx
@@ -1,13 +1,25 @@
 export default defineComponent({
   setup() {
     return () => (
-      <div h-100vh flex="~ col" justify-center items-center>
-        <div text-6xl>0:000</div>
-        <div my-24 text-center>
-          <div>不是哥吹，一把就过！</div>
-          <div>最佳成绩 <span color-orange>9:991</span></div>
+      <div h-100vh flex="~ col">
+        <div text-right p-2 mb-2>
+          <button
+            p-2
+            rd="50%"
+            bg="dark:gray/40 gray/20"
+            icon-btn
+            onClick={() => toggleDark()}>
+            <div dark:i-carbon-moon i-carbon-sun text-xl />
+          </button>
         </div>
-        <div p-13 rd="50%" bg-white color-black>开始</div>
+        <div flex="~ col" justify-center items-center>
+          <div text-6xl>0:000</div>
+          <div my-24 text-center>
+            <div>不是哥吹，一把就过！</div>
+            <div>最佳成绩 <span color-orange>9:991</span></div>
+          </div>
+          <div p-13 rd="50%" bg-white color-black>开始</div>
+        </div>
       </div>
     )
   },
