3084fed6a13d143f3a5c5976d12190a1498effd5
chang <2622310479@qq.com>
2023-01-18 13:49:23 +0800

    chore: 添加 Unocss Attributes JSX 类型支持注释

--- shims.d.ts
+++ shims.d.ts
@@ -6,6 +6,11 @@ declare module '*.vue' {
   export default component
 }
 
+/**
+ * 解决部分 Unocss Attributes 模式下 TypeScript 检查报错问题
+ * 解决方法 {@link https://github.com/unocss/unocss/tree/main/packages/preset-attributify/#vue-3}
+ * 问题链接 {@link https://github.com/unocss/unocss/issues/742}
+ */
 declare module '@vue/runtime-dom' {
   interface HTMLAttributes extends AttributifyAttributes {}
 }
