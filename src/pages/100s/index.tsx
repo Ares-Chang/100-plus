8b3b88717d14d3661be44f63bec29676f50a6cbc
chang <2622310479@qq.com>
2023-01-18 11:26:37 +0800

    feat(100s): 页面构建

--- src/pages/100s/index.tsx
+++ src/pages/100s/index.tsx
@@ -1,10 +1,14 @@
-import { NButton } from 'naive-ui'
-
 export default defineComponent({
   setup() {
-    return () => <div>
-      {[1, 2, 3, 4, 5].map(item => <div color-pink text-2xl>{item}</div>)}
-      <NButton type="success">test</NButton>
-    </div>
+    return () => (
+      <div h-100vh flex="~ col" justify-center items-center>
+        <div text-6xl>0:000</div>
+        <div my-24 text-center>
+          <div>不是哥吹，一把就过！</div>
+          <div>最佳成绩 <span color-orange>9:991</span></div>
+        </div>
+        <div p-13 rd="50%" bg-white color-black>开始</div>
+      </div>
+    )
   },
 })
