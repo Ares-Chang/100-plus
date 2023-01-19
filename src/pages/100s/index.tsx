d798f796b8166dfbe9839cf986f96e69393cca5e
chang <2622310479@qq.com>
2023-01-19 20:21:01 +0800

    feat(100s): create game start logic

--- src/pages/100s/index.tsx
+++ src/pages/100s/index.tsx
@@ -1,5 +1,19 @@
 export default defineComponent({
   setup() {
+    let time = $ref(0)
+    let timer: ReturnType<typeof setInterval> | null = null
+
+    function handleBtn() {
+      if (!timer) {
+        time = 0
+        timer = setInterval(() => time++, 1)
+      }
+      else {
+        clearInterval(timer)
+        timer = null
+      }
+    }
+
     return () => (
       <div h-100vh flex="~ col">
         <div text-right p-2 mb-2>
@@ -13,12 +27,18 @@ export default defineComponent({
           </button>
         </div>
         <div flex="~ col" justify-center items-center>
-          <div text-6xl>0:000</div>
+          <div text-6xl>{ time }</div>
           <div my-24 text-center>
             <div>不是哥吹，一把就过！</div>
             <div>最佳成绩 <span color-orange>9:991</span></div>
           </div>
-          <div p-13 rd="50%" bg="dark:white black" color="dark:black white">开始</div>
+          <div
+            p-13 rd="50%"
+            bg="dark:white black"
+            color="dark:black white"
+            onClick={handleBtn}>
+            { timer ? '停止' : '开始' }
+          </div>
         </div>
       </div>
     )
