3d9d35a32edcdb74bc3e2c36a4cc8f6cea97669a
chang <2622310479@qq.com>
2023-04-30 16:01:23 +0800

    feat: change time random num page rule

--- src/pages/time/index.vue
+++ src/pages/time/index.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import generatedRoutes from 'virtual:generated-pages'
+
 useSetTitle('Clock')
+
+const list = generatedRoutes.filter(item => item.path.includes('time/'))
 /**
  * show random clock project page
  */
-const num = useRandomInt(1, 2)
+const num = useRandomInt(0, list.length - 1)
 </script>
 
 <template>
-  <iframe w-100vw h-100vh :src="`/time/${num}`" frameborder="0" />
+  <iframe w-100vw h-100vh :src="list[num].path" frameborder="0" />
 </template>
