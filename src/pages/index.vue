32ecfba38d700ad7688a8b6292e09ed9a39d1348
chang <2622310479@qq.com>
2023-05-02 16:26:13 +0800

    chore: Project arrangement

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,52 @@
 <script setup lang="ts">
 const list = [
   {
-    name: '123',
-    age: 12,
+    name: '扫雷',
+    link: 'https://mines.areschang.top/',
   },
   {
-    name: '456',
-    age: 13,
+    name: '时钟',
+    link: 'https://time.areschang.top/time',
+  },
+  {
+    name: '网易云音乐 API',
+    link: 'http://api.music.areschang.top/',
+  },
+  {
+    name: '木鱼',
+    link: 'https://wfish.areschang.top/',
+  },
+  {
+    name: '人生小格',
+    link: 'https://life-grid.areschang.top/',
+  },
+  {
+    name: 'CDN Vue Develop Repo',
+    link: 'https://cdn-vue.areschang.top/',
+  },
+  {
+    name: 'ChatGPT-Next-Web',
+    link: 'https://chat.areschang.top/',
+  },
+  {
+    name: 'H5 打开微信小程序',
+    link: 'https://open-applet.areschang.top/',
+  },
+  {
+    name: '挑战 100 秒',
+    link: 'https://100.areschang.top/100s',
+  },
+  {
+    name: '滚动文字',
+    link: 'https://100.areschang.top/scrollText',
+  },
+  {
+    name: '笑话段子',
+    link: 'https://100.areschang.top/joke',
+  },
+  {
+    name: '日历',
+    link: 'https://100.areschang.top/calendar',
   },
 ]
 </script>
@@ -15,7 +55,7 @@ const list = [
   <div>
     <div v-for="item in list" :key="item.name">
       <div>{{ item.name }}</div>
-      <div>{{ item.age }}</div>
+      <div>{{ item.link }}</div>
     </div>
   </div>
 </template>
@@ -23,4 +63,4 @@ const list = [
 <route lang="yaml">
 meta:
   layout: home
-  </route>
+</route>
