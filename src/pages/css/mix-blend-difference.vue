439ab6a11d0a55898bae15a93e47b1b841c832cd
Chang <AresChang114@qq.com>
2025-01-22 15:51:47 +0800

    feat: mix-blend-difference 文字背景混合

---
+++ src/pages/css/mix-blend-difference.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    from="black 50%" to="white 50%"
+    h-screen flex items-center justify-center
+    bg-gradient-to-tr
+  >
+    <div
+      bg="gray-50/20"
+      cursor-pointer rd-lg p-4
+      text-center text-5xl font-bold
+      mix-blend-difference transition-400
+      hover:translate-x--30
+    >
+      文字与背景混合
+      <p mt-3>
+        mix-blend-difference
+      </p>
+    </div>
+  </div>
+</template>
